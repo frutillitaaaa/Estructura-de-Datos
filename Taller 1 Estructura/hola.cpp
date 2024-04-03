018dcd27fcfff7f21135fdb1e4197056d58a3eff
frutillitaaaa <119687288+frutillitaaaa@users.noreply.github.com>
2024-04-03 18:15:59 -0300

    Add files via upload

---
+++ Taller 1 Estructura/hola.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"hola"<<std::endl;
+    system("pause");
+    return 0;
+}
